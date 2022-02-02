de97d8241dbec9ccc47a633854e92676fb4808b6
Victor Gaydov <victor@enise.org>
2022-02-02 20:06:00 +0300

    Fix endian

--- src/Strings.cpp
+++ src/Strings.cpp
@@ -13,7 +13,12 @@ std::string CodeToString(UInt32 value)
 
     std::stringstream ss;
 
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     ss << "'" << bytes[0] << bytes[1] << bytes[2] << bytes[3] << "'";
+#else
+    ss << "'" << bytes[3] << bytes[2] << bytes[1] << bytes[0] << "'";
+#endif
+
     ss << " (0x" << std::hex << value << ")";
 
     return ss.str();
